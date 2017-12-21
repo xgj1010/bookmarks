dfe13b265117d0235e2e6ab54dd643ac1f35f3a3
xgj1010 <xgj891010>
2017-12-21 13:03:58 +0800

    修改登录

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # url(r'^login/$', user_login, name='login'),
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
-    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout-then-login'),
+    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
     url(r'^$', dashboard, name='dashboard'),
 ]
